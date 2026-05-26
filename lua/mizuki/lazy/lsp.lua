acb18612ef589582627da3273aef4720e1bf6c94
Borinees <miarochaborin@gmail.com>
2026-05-26 14:45:44 -0300

    Setup lsp

---
+++ lua/mizuki/lazy/lsp.lua
@@ -0,0 +1,142 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "stevearc/conform.nvim",
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/nvim-cmp",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
+        "mfussenegger/nvim-jdtls",
+    },
+
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+            }
+        })
+        local cmp = require('cmp')
+        local cmp_lsp = require("cmp_nvim_lsp")
+        local capabilities = vim.tbl_deep_extend(
+            "force",
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            cmp_lsp.default_capabilities())
+
+        require("fidget").setup({})
+        require("mason").setup()
+        require("mason-lspconfig").setup({
+            ensure_installed = {
+                "rust_analyzer",
+                "lua_ls",
+                "gopls",
+                "vtsls",
+                "tailwindcss",
+                "angularls",
+                "jdtls",
+            },
+
+            handlers = {
+                function(server_name)
+                    if server_name == "jdtls" then
+                        return
+                    end
+                  
+                    require("lspconfig")[server_name].setup ({
+                        capabilities = capabilities
+                    })
+                end,
+                ["lua_ls"] = function()
+                    local lspconfig = require("lspconfig")
+
+                    lspconfig.lua_ls.setup {
+                        capabilities = capabilities,
+                        settings = {
+                            Lua = {
+                                runtime = {
+                                    version = 'LuaJIT',
+                                },
+                                diagnostics = {
+                                    globals = { 'vim' },
+                                },
+                                workspace = {
+                                    library = vim.api.nvim_get_runtime_file("", true),
+                                    checkThirdParty = false,
+                                },
+                                format = {
+                                    enable = true,
+                                    defaultConfig = {
+                                        indent_style = "space",
+                                        indent_size = "2",
+                                    }
+                                },
+                            }
+                        }
+                    }
+                end,
+                ["tailwindcss"] = function()
+                    local lspconfig = require("lspconfig")
+                    lspconfig.tailwindcss.setup({
+                        capabilities = capabilities,
+                        filetypes = { "html", "css", "scss", "javascript", "javascriptreact", "typescript","typescriptreact", "vue", "svelte", "heex" },
+                    })
+                end,
+                ["jdtls"] = function()
+                    local jdtls = require("jdtls")
+                       
+                    jdtls.start_or_attach({
+                        capabilities = capabilities,
+                        cmd = {"jdtls"},
+                        root_dir = vim.fs.root(0, {'gradlew', '.git', 'mvnw'}),
+                       
+                        settings = {
+                            java = {},
+                        },
+                        init_options = {
+                            bundles = {},
+                        },
+                    })
+                end,
+            }
+        })
+
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    require('luasnip').lsp_expand(args.body)
+                end,
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ["<C-Space>"] = cmp.mapping.complete(),
+            }),
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+            }, {
+                { name = 'buffer' },
+            })
+        })
+
+        vim.diagnostic.config({
+            float = {
+                focusable = false,
+                style = "minimal",
+                border = "rounded",
+                source = "always",
+                header = "",
+                prefix = "",
+            },
+        })
+
+    end
+}
